2cd46d5079b77b423b0e93c9e7a2ca6768655145
Hervé Beraud <hberaud@redhat.com>
2024-02-20 16:17:34 +0100

    backup

--- python/atomic.py
+++ python/atomic.py
@@ -1,4 +1,20 @@
+import socket
+import requests
 n = 0
 
 def foo():
+    global n
     n += 1
+
+with open("/tmp/test", "+w") as test:
+    test.write("ok")
+
+# create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# now connect to the web server on port 80 - the normal http port
+s.connect(("www.python.org", 80))
+
+with open("/tmp/boom", '+w') as boom:
+    boom.write("ok")
+
+r = requests.get("https://google.com")
